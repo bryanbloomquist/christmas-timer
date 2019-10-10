d7a4ba42ab31ddae969310c557a22619f4c05670
bloomy215 <bmbloomquist@gmail.com>
2019-10-10 10:31:39 -0500

    js was not functioning on iOS devices, reset to basic until fixed

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,23 +1,38 @@
+let year, month, day, deadline;
+
 let interval;
 let eventdate = "12-25";
 let eventname = "Christmas";
 
-const checkRadio = ( radio ) => {
-  eventdate = radio.value.slice( 0, 5 );
-  eventname = radio.value.slice( 6 );
-  clearInterval( interval );
-  getDate( eventdate, eventname );
-}
+// const checkRadio = ( radio ) => {
+//   eventdate = radio.value.slice( 0, 5 );
+//   eventname = radio.value.slice( 6 );
+//   clearInterval( interval );
+//   getDate( eventdate, eventname );
+// }
 
-const getDate = ( date, name ) => {
-  let month = date.slice( 0, 2 )
-  let day = date.slice( 3, 5 );
-  let year = new Date().getFullYear();
-  currentdate = new Date().toJSON().slice( 5, 10 )
-  if ( currentdate >= date ) { year += 1; }
-  document.getElementById( "event" ).innerHTML = name;
+// const getDate = ( date, name ) => {
+//   let month = date.slice( 0, 2 )
+//   let day = date.slice( 3, 5 );
+//   let year = new Date().getFullYear();
+//   currentdate = new Date().toJSON().slice( 5, 10 )
+//   if ( currentdate >= date ) { year += 1; }
+//   document.getElementById( "event" ).innerHTML = name;
+//   document.getElementById( "year" ).innerHTML = year;
+//   let deadline = new Date( month + " " + day + ", " + year + " 00:00:00" );
+//   startClock( deadline );
+// }
+
+const getDate = () => {
+  date = new Date().getDate();
+  month = new Date().getMonth() + 1;
+  year = new Date().getFullYear();
+  console.log( date, month, year );
+  if ( month === 12 && day >=25 ) {
+    year +=1;
+  }
   document.getElementById( "year" ).innerHTML = year;
-  let deadline = new Date( month + " " + day + ", " + year + " 00:00:00" );
+  deadline = new Date( "Dec 25, " + year + " 00:00:00" );
   startClock( deadline );
 }
 
@@ -47,4 +62,5 @@ const remainingTime = ( deadline ) => {
   };
 }
 
-getDate( eventdate, eventname );
+// getDate( eventdate, eventname );
+getDate();
